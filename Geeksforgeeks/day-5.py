dc14a577e1aa7f871f2410b7be2126abd6797ee8
kunalaswar <kunalaswar2004@gmail.com>
2025-04-09 18:17:21 +0530

    day-5

---
+++ Geeksforgeeks/day-5.py
@@ -0,0 +1,34 @@
+
+# Array Subset
+
+# from  collections import Counter
+# class Solution:
+#     #Function to check if a is a subset of b.
+#     def isSubset(self, a, b):
+#         # Your code here
+#         freq_a = Counter(a)
+#         freq_b = Counter(b)
+#         for val in freq_b:
+#             if freq_b[val]>freq_a.get(val,0):
+#                 return False
+#         return True        
+        
+    
+# s = Solution()
+# res = s.isSubset([11, 7, 1, 13, 21, 3, 7, 3], [11, 3, 7, 1, 7])
+# print(res)
+
+
+
+# freq_a = {1:1, 2:2, 3:1}
+
+# freq_b = {2:2}
+
+# Loop:
+
+# val = 2
+
+# Is freq_b[2] > freq_a[2]? → Is 2 > 2? NO  
+# so that it is ok right 
+
+    
