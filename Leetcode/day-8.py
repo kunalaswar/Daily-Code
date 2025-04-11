efc116926dab8fd7bb63fc298d568aa26cb64a00
kunalaswar <kunalaswar2004@gmail.com>
2025-04-11 20:24:07 +0530

    day-8

--- Leetcode/day-8.py
+++ Leetcode/day-8.py
@@ -1,22 +1,23 @@
-# 20. Valid Parentheses
+# # 20. Valid Parentheses
 
-class Solution:
-    def isValid(self, s: str) -> bool:
-        lst = []
-        bracket_dict = {')': '(', '}': '{', ']': '['}
-        for char in s:
-            if char in bracket_dict:
-                top_ele = lst.pop() if lst else "_"
-                if bracket_dict[char] !=top_ele:
-                    return False
-            else:
-                lst.append(char)        
-        return not lst
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         lst = []
+#         bracket_dict = {')': '(', '}': '{', ']': '['}
+#         for char in s:
+#             if char in bracket_dict:
+#                 top_ele = lst.pop() if lst else "_"
+#                 if bracket_dict[char] !=top_ele:
+#                     return False
+#             else:
+#                 lst.append(char)        
+#         return not lst
 
 
-s = Solution()
+# s = Solution()
 
-print(s.isValid("()"))        # True
-print(s.isValid("()[]{}"))    # True
-print(s.isValid("(]"))        # False
-print(s.isValid("([)]"))      # False
+# print(s.isValid("()"))        # True
+# print(s.isValid("()[]{}"))    # True
+# print(s.isValid("(]"))        # False
+# print(s.isValid("([)]"))      # False
+
