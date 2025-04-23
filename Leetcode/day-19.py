6beb372dc535ce7dbf0735f4846a9d5150cc4621
kunalaswar <kunalaswar2004@gmail.com>
2025-04-23 23:26:00 +0530

    day-19

--- Leetcode/day-19.py
+++ Leetcode/day-19.py
@@ -14,3 +14,36 @@
 
 
 #OR 
+
+# def nextGreatestLetter(letters,target):
+#     i = 0
+#     while(i < len(letters)):
+#         if letters[i]>target:
+#             return letters[i]
+#         i = i+1
+#     return letters[0]       
+
+# letters =["c","f","j"]
+# target = "a"
+# s = nextGreatestLetter(letters,target)
+# print(s)
+
+#OR
+# Binary search Approach
+
+def nextGreatestLetter(letters,target):
+    start = 0
+    end = len(letters)-1
+    while start<=end:
+        mid = (start + end)//2
+        if letters[mid] >target:
+            end = mid -1
+        else:
+            start = mid +1
+
+    return letters[start %len(letters)]            
+
+letters =["c","f","j"]
+target = "a"
+s = nextGreatestLetter(letters,target)
+print(s)
