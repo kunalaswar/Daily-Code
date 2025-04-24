3c47c13f7e3815b5dc66290f29208357e6b493d5
kunalaswar <kunalaswar2004@gmail.com>
2025-04-24 20:27:57 +0530

    day-19

--- Geeksforgeeks/day-19.py
+++ Geeksforgeeks/day-19.py
@@ -1,17 +1,17 @@
 
-# Rotating an Array
+# # Rotating an Array
 
-class Solution:
-    def leftRotate(self, arr, d):
-        # code
-        n = len(arr)
-        temp = arr[:d]
-        for i in range(n - d):
-            arr[i] = arr[i + d]
-        arr[n - d:] = temp
-        return arr
+# class Solution:
+#     def leftRotate(self, arr, d):
+#         # code
+#         n = len(arr)
+#         temp = arr[:d]
+#         for i in range(n - d):
+#             arr[i] = arr[i + d]
+#         arr[n - d:] = temp
+#         return arr
     
 
-s = Solution()    
-res = s.leftRotate([-1, -2, -3, 4, 5, 6, 7],2)
-print(res)
+# s = Solution()    
+# res = s.leftRotate([-1, -2, -3, 4, 5, 6, 7],2)
+# print(res)
