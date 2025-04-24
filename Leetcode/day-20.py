f4142aa5924982e4b8ed5839676ce6826f8558c9
kunalaswar <kunalaswar2004@gmail.com>
2025-04-24 18:09:19 +0530

    day-20

---
+++ Leetcode/day-20.py
@@ -0,0 +1,18 @@
+
+# 349. Intersection of Two Arrays
+
+# class Solution:
+#     def intersection(self, nums1,nums2):
+#         lst = []
+#         for i in range(len(nums1)):
+#             for j in range(len(nums2)):
+#             # print(nums1[i])
+#                 if nums1[i]==nums2[j] and nums1[i] not in lst:
+#                     lst.append(nums1[i])
+#         return lst
+
+# s = Solution()
+# res = s.intersection( [1,2,2,1], [2,2])
+# print(res)
+
+
