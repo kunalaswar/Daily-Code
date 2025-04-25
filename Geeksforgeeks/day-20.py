43317fd2b633876b34a1a099930acce31517c92d
kunalaswar <kunalaswar2004@gmail.com>
2025-04-25 15:35:55 +0530

    day-20

--- Geeksforgeeks/day-20.py
+++ Geeksforgeeks/day-20.py
@@ -1,35 +1,35 @@
 
-#   Implement stack using array
+# #   Implement stack using array
 
-class MyStack:
-    def __init__(self):
-        self.arr=[]
+# class MyStack:
+#     def __init__(self):
+#         self.arr=[]
     
-    def push(self,data):
-        self.arr.append(data)
+#     def push(self,data):
+#         self.arr.append(data)
         
-    def pop(self):
-        if len(self.arr) == 0:
-            return -1
-        return self.arr.pop()
+#     def pop(self):
+#         if len(self.arr) == 0:
+#             return -1
+#         return self.arr.pop()
     
 
-query = '1,2,1,3,2,1,4,2'  
-tokens = list(map(int,query.split(',')))
+# query = '1,2,1,3,2,1,4,2'  
+# tokens = list(map(int,query.split(',')))
 
-stack = MyStack()
-output = []
+# stack = MyStack()
+# output = []
 
-i = 0
-while i < len(tokens):
-    if tokens[i]==1:
-        stack.push(tokens[i+1])
-        i = i+2
-    elif tokens[i]==2:
-        output.append(stack.pop())    
-        i = i+1
+# i = 0
+# while i < len(tokens):
+#     if tokens[i]==1:
+#         stack.push(tokens[i+1])
+#         i = i+2
+#     elif tokens[i]==2:
+#         output.append(stack.pop())    
+#         i = i+1
 
-print(",".join(map(str,output)))
+# print(",".join(map(str,output)))
 
 
 
