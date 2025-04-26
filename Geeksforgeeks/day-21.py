4e705ec7ae12acce9199879ba7b4fd947d579da8
kunalaswar <kunalaswar2004@gmail.com>
2025-04-26 16:36:56 +0530

    day-21

--- Geeksforgeeks/day-21.py
+++ Geeksforgeeks/day-21.py
@@ -1,1 +1,16 @@
 
+# Find element at a given Index
+
+# class Solution:
+#     def findElementAtIndex(self, key,arr):
+#         # code here
+#         for i in range(len(arr)):
+#             if i == key:
+#                 return arr[i]
+        
+# arr  = [10, 20, 30, 40, 50]        
+# s = Solution()        
+# res = s.findElementAtIndex(2,arr)
+# print(res)
+
+
