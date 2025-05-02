2ba5912d6bf101270b6417da5da1b9b94e357c7b
kunalaswar <kunalaswar2004@gmail.com>
2025-05-02 20:39:14 +0530

    update

--- Geeksforgeeks/day-23.py
+++ Geeksforgeeks/day-23.py
@@ -1,25 +1,25 @@
 
-#Queue Using Array
+# #Queue Using Array
 
-class MyQueue:
+# class MyQueue:
 
-    def __init__(self):
-        self.queue = []
+#     def __init__(self):
+#         self.queue = []
 
-    def push(self, x):
-        self.queue.append(x)
-    def pop(self): 
-        if len(self.queue) == 0:
-            return -1  
-        return self.queue.pop(0) 
+#     def push(self, x):
+#         self.queue.append(x)
+#     def pop(self): 
+#         if len(self.queue) == 0:
+#             return -1  
+#         return self.queue.pop(0) 
 
 
-q= MyQueue()
-q.push(10)
-q.push(20)
+# q= MyQueue()
+# q.push(10)
+# q.push(20)
 
-print(q.pop())  
-print(q.pop())  
-print(q.pop())  
+# print(q.pop())  
+# print(q.pop())  
+# print(q.pop())  
 
 
