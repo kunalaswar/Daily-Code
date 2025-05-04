d0cfde168b75cb8d3250b9e72352df847b0e8447
kunalaswar <kunalaswar2004@gmail.com>
2025-05-04 08:20:51 +0530

    day-25

--- Geeksforgeeks/day-25.py
+++ Geeksforgeeks/day-25.py
@@ -1,18 +1,18 @@
 
-# Swap kth elements
+# # Swap kth elements
 
 
-def swapKth(arr, k):
-        # Code Here
-    for i in range(len(arr)):
-        n = len(arr)
-        if k<=0 and k>n:
-            return arr
-        arr[k-1],arr[n-k] = arr[n-k],arr[k-1]        
-        return arr
+# def swapKth(arr, k):
+#         # Code Here
+#     for i in range(len(arr)):
+#         n = len(arr)
+#         if k<=0 and k>n:
+#             return arr
+#         arr[k-1],arr[n-k] = arr[n-k],arr[k-1]        
+#         return arr
     
-res = swapKth([1, 2, 3, 4, 5], 2) 
-print(res)
+# res = swapKth([1, 2, 3, 4, 5], 2) 
+# print(res)
         
-res1 = swapKth([1, 2, 3, 4, 5, 6, 7, 8], 3)
-print(res1)
+# res1 = swapKth([1, 2, 3, 4, 5, 6, 7, 8], 3)
+# print(res1)
