45019810afe71906b2adc9b20806eb1a2ab43f1c
kunalaswar <kunalaswar2004@gmail.com>
2025-05-07 21:26:32 +0530

    day-27

---
+++ Geeksforgeeks/day-27.py
@@ -0,0 +1,13 @@
+
+class Solution:
+    def leftRotate(self, arr, k):
+        # Your code goes here
+        n = len(arr)
+        k = k % n
+        arr[:] = arr[k:] + arr[:k]
+        return arr
+        
+s = Solution()        
+# print(s.leftRotate([1, 2, 3, 4, 5, 6, 7], 2)) 
+print(s.leftRotate([1, 2, 3, 4, 5, 6], 12)) 
+print(s.leftRotate([1, 2, 3, 4, 5, 6], 11)) 
