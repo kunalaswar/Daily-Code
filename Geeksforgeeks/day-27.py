55db59c7dfb89d886819d683ba669139f7de3bcc
kunalaswar <kunalaswar2004@gmail.com>
2025-05-07 21:27:06 +0530

    day-27

--- Geeksforgeeks/day-27.py
+++ Geeksforgeeks/day-27.py
@@ -1,13 +1,16 @@
 
-class Solution:
-    def leftRotate(self, arr, k):
-        # Your code goes here
-        n = len(arr)
-        k = k % n
-        arr[:] = arr[k:] + arr[:k]
-        return arr
+#! Quick Left Rotation
+# class Solution:
+#     def leftRotate(self, arr, k):
+#         # Your code goes here
+#         n = len(arr)
+#         k = k % n
+#         arr[:] = arr[k:] + arr[:k]
+#         return arr
         
-s = Solution()        
-# print(s.leftRotate([1, 2, 3, 4, 5, 6, 7], 2)) 
-print(s.leftRotate([1, 2, 3, 4, 5, 6], 12)) 
-print(s.leftRotate([1, 2, 3, 4, 5, 6], 11)) 
+# s = Solution()        
+# # print(s.leftRotate([1, 2, 3, 4, 5, 6, 7], 2)) 
+# print(s.leftRotate([1, 2, 3, 4, 5, 6], 12)) 
+# print(s.leftRotate([1, 2, 3, 4, 5, 6], 11)) 
+
+
