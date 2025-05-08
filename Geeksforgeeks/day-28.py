bd4e113ef940dac5347a95f7e5e36f0f09bdfb3e
kunalaswar <kunalaswar2004@gmail.com>
2025-05-08 09:05:16 +0530

    day-28

--- Geeksforgeeks/day-28.py
+++ Geeksforgeeks/day-28.py
@@ -1,12 +1,16 @@
 
-class Solution:
-    def check_elements(self, arr, n, A, B):
-        # Your code goes here
-        for num in range(A, B + 1):
-            if num not in arr:
-                return False
-        return True
+# Elements in the Range
 
-sol = Solution()
-print(sol.check_elements([1, 4, 5, 2, 7, 8, 3], 7, 2, 5)) 
-print(sol.check_elements([1, 4, 5, 2, 7, 8, 3], 7, 2, 6))  
+# class Solution:
+#     def check_elements(self, arr, n, A, B):
+#         # Your code goes here
+#         for num in range(A, B + 1):
+#             if num not in arr:
+#                 return False
+#         return True
+
+# sol = Solution()
+# print(sol.check_elements([1, 4, 5, 2, 7, 8, 3], 7, 2, 5)) 
+# print(sol.check_elements([1, 4, 5, 2, 7, 8, 3], 7, 2, 6))  
+
+
