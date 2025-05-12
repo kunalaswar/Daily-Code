b38de1f1dd415dada81aafb7c5ed64ef4b86fa2c
kunalaswar <kunalaswar2004@gmail.com>
2025-05-12 21:33:45 +0530

    update

--- Geeksforgeeks/day-30.py
+++ Geeksforgeeks/day-30.py
@@ -1,14 +1,14 @@
 #  First 1 in a Sorted Binary Array
 
-# class Solution:
-#     def firstIndex(self, arr):
-#                 for i in range(len(arr)):
-#                  if arr[i] == 1:
-#                   return i
+class Solution:
+    def firstIndex(self, arr):
+                for i in range(len(arr)):
+                 if arr[i] == 1:
+                  return i
                 
-#                 return -1
+                return -1
 
-# sol = Solution()
-# print(sol.firstIndex([0, 0, 0, 0, 0, 0, 1, 1, 1, 1]))
-# print(sol.firstIndex([0, 0, 0, 0])) 
+sol = Solution()
+print(sol.firstIndex([0, 0, 0, 0, 0, 0, 1, 1, 1, 1]))
+print(sol.firstIndex([0, 0, 0, 0])) 
 
