8a1eb9203d8239c03c38791ee0e2e7a3d38d733b
kunalaswar <kunalaswar2004@gmail.com>
2025-05-14 07:00:12 +0530

    day-32

---
+++ Geeksforgeeks/day-32.py
@@ -0,0 +1,13 @@
+
+# Maximum product of two numbers
+
+
+def maxProduct(arr):
+		arr.sort()
+		return max(arr[0] * arr[1] , arr[-1] * arr[-2])
+
+arr = [1, 4, 3, 6, 7, 0] 
+print(maxProduct(arr))		   
+	            
+arr= [1, 100, 42, 4, 23]
+print(maxProduct(arr))		   
