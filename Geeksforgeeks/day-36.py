42b7ec2b42b5975d0c4898bbf21b7e3a95e10231
kunalaswar <kunalaswar2004@gmail.com>
2025-05-18 07:46:46 +0530

    day-36

---
+++ Geeksforgeeks/day-36.py
@@ -0,0 +1,10 @@
+# Reverse sub array
+
+def reverseSubArray(arr,l,r):
+		arr[l-1:r]=arr[l-1:r][::-1]
+		print(arr)
+		
+
+reverseSubArray( [1, 2, 3, 4, 5, 6, 7], 2,  4)
+
+reverseSubArray( [1, 6, 7, 4], 1, 4)
