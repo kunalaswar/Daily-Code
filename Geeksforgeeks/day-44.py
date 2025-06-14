fb82c7cf8309de23381a5eced432955d6115f588
kunalaswar <kunalaswar2004@gmail.com>
2025-06-14 19:42:32 +0530

    day-44

---
+++ Geeksforgeeks/day-44.py
@@ -0,0 +1,14 @@
+#   Count pair sum
+
+def countPairs(arr1, arr2, x):
+    s = set(arr2)  # put all elements of arr2 in a set for fast lookup
+    count = 0
+
+    for a in arr1:
+        if (x - a) in s:
+            count += 1
+
+    return count
+        
+print(countPairs([1, 3, 5, 7], [2, 3, 5, 8], 10))  # Output: 2
+print(countPairs([1, 2, 3, 4], [5, 6, 7, 8], 5))   # Output: 0
