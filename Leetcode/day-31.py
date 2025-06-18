f4669b4cbdd75eb253a7f0473751e46c0c8b2e05
kunalaswar <kunalaswar2004@gmail.com>
2025-06-18 20:46:54 +0530

    day-31

---
+++ Leetcode/day-31.py
@@ -0,0 +1,23 @@
+
+# 412. Fizz Buzz
+class Solution:
+    def fizzBuzz(self, n):
+        lst = []
+        for i in range(1, n+1):
+                
+            if i%3==0 and i%5==0:
+                lst.append("FizzBuzz")
+            elif i%3 ==0:
+                lst.append("Fizz")
+                     
+            elif i%5 ==0:
+                lst.append("Buzz")
+            else:
+                lst.append(str(i))
+
+        return lst          
+
+s = Solution()
+print(s.fizzBuzz(3))
+print(s.fizzBuzz(5))
+print(s.fizzBuzz(15))
