d725f06e471e8c8525c2a0457e38a1aabdf4a294
kunalaswar <kunalaswar2004@gmail.com>
2025-06-19 22:23:27 +0530

    day-49

---
+++ Geeksforgeeks/day-49.py
@@ -0,0 +1,24 @@
+
+# Largest product
+
+class Solution:
+    def findMaxProduct(self, arr, k):
+        n = len(arr)
+        if k > n:
+            return 0  # not enough elements
+
+        max_product = 0
+
+        for i in range(n - k + 1):
+            product = 1
+            for j in range(i, i + k):
+                product *= arr[j]
+            max_product = max(max_product, product)
+
+        return max_product
+
+
+
+s = Solution()
+print(s.findMaxProduct([1, 2, 3, 4], 2))   # Output: 12
+print(s.findMaxProduct([1, 6, 7, 8], 3))   # Output: 336
