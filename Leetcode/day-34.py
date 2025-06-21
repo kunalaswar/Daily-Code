8090f69ff41e0e1268fa0715f5c09fbd08cc10f2
kunalaswar <kunalaswar2004@gmail.com>
2025-06-21 19:44:43 +0530

    day-34

---
+++ Leetcode/day-34.py
@@ -0,0 +1,21 @@
+
+# 2520. Count the Digits That Divide a Number
+
+class Solution:
+    def countDigits(self, num: int) -> int:
+        count = 0
+        temp = num
+        while(temp > 0):
+            r = temp %10
+            if num %r==0: 
+                count = count +1
+            temp = temp//10    
+
+            
+        return count
+
+s = Solution()    
+print(s.countDigits(7))
+print(s.countDigits(121))
+print(s.countDigits(1248))
+
