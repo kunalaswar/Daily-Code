e98f0be9b17fa5f322dd9544d3e75529de5dd20c
kunalaswar <kunalaswar2004@gmail.com>
2025-06-26 21:16:11 +0530

    day-39

---
+++ Leetcode/day-39.py
@@ -0,0 +1,23 @@
+
+# 231. Power of Two
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+            
+        # base case
+        if n<=0:
+            return False
+        if n==1:
+            return True
+        if n%2!=0:
+            return False
+        # Recursive case
+        return self.isPowerOfTwo(n//2)          
+        
+s = Solution()
+print(s.isPowerOfTwo(1))
+print(s.isPowerOfTwo(16))
+print(s.isPowerOfTwo(3  ))
+
+
+        
