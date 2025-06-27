8c54a4ad9d0b3c03147e2c2cd3f917562ab25074
kunalaswar <kunalaswar2004@gmail.com>
2025-06-27 19:37:20 +0530

    day-57

--- Geeksforgeeks/day-57.py
+++ Geeksforgeeks/day-57.py
@@ -8,7 +8,7 @@ class Solution:
         # lst.sort(reverse= True)
         for i in range(len(lst)):
             for j in range(0,len(lst)-1):
-                if lst[j] < lst[j+1]: # ascending order 
+                if lst[j] < lst[j+1]: # descending order 
                     lst[j],lst[j+1] = lst[j+1],lst[j]
 
 
@@ -21,3 +21,20 @@ print(s.ReverseSort("geeks"))
 print(s.ReverseSort("for"))
 
         
+# OR 
+
+
+# class Solution:
+#     def ReverseSort(self, s): 
+#         lst = []
+#         for i in s:
+#             lst.append(i)
+#         lst.sort(reverse= True)
+
+#         return ''.join(lst)
+        
+# s  = Solution()        
+# print(s.ReverseSort("geeks"))
+
+# print(s.ReverseSort("for"))        
+                
