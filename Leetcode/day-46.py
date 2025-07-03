28d5edb0d9e72eff7124e9b25d6b968cd9ee5c3d
kunalaswar <kunalaswar2004@gmail.com>
2025-07-03 14:52:48 +0530

    day-46

---
+++ Leetcode/day-46.py
@@ -0,0 +1,34 @@
+
+# 705. Design HashSet
+class MyHashSet:
+
+    def __init__(self):
+        self.size = 10**6  + 1
+        self.data = [False] * self.size
+
+        
+
+    def add(self, key: int) -> None:
+        self.data[key] = True
+        
+
+    def remove(self, key: int) -> None:
+        self.data[key] = False
+        
+
+    def contains(self, key: int) -> bool:
+        return self.data[key]
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
+
+mySet = MyHashSet()
+mySet.add(5)
+print(mySet.contains(5))   
+mySet.remove(5)
+print(mySet.contains(5))   
