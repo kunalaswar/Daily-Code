8764aaff676d4a0d7d6f03c7530ac22463da827f
kunalaswar <kunalaswar2004@gmail.com>
2025-07-09 19:49:34 +0530

    day-52

---
+++ Leetcode/day-52.py
@@ -0,0 +1,24 @@
+
+# 
+
+class Solution:
+    def maxSubArray(self, nums):
+        curr_sum = 0
+        max_sum = nums[0]
+        for i in range(len(nums)):
+            curr_sum += nums[i]
+            if curr_sum > max_sum :
+                max_sum = curr_sum
+            if curr_sum < 0:
+                curr_sum  = 0
+
+        return max_sum  
+
+
+s = Solution()
+print(s.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+
+
+print(s.maxSubArray([1]))
+
+print(s.maxSubArray([5,4,-1,7,8]))
