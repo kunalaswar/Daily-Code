b8dbfa30230e6bda849d817cdcc551ca8c3d8ee9
kunalaswar <kunalaswar2004@gmail.com>
2025-07-20 07:37:42 +0530

    day-72

--- Geeksforgeeks/day-72.py
+++ Geeksforgeeks/day-72.py
@@ -1,2 +1,19 @@
 
 # 
+
+class Solution:
+    def removeDuplicates(self, arr):
+        if not arr:
+            return []
+
+        result = [arr[0]]  # First element is always unique in a sorted array
+
+        for i in range(1, len(arr)):
+            if arr[i] != arr[i - 1]:
+                result.append(arr[i])
+
+        return result
+
+s = Solution()
+print(s.removeDuplicates([2, 2, 2, 2, 2]))
+print(s.removeDuplicates([1, 2, 4]))
