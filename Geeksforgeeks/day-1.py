0db538e518cabb7af37836f7900581f488f58047
kunalaswar <kunalaswar2004@gmail.com>
2026-02-08 08:04:47 +0530

    Q-1

--- Geeksforgeeks/day-1.py
+++ Geeksforgeeks/day-1.py
@@ -1,16 +1,23 @@
-#largest Element in Array
-from typing import List
+# #largest Element in Array
+# from typing import List
 
-class Solution:
-    def largest(self, arr):   
-        max_val = arr[0]
-        for val in arr:
-            if val > max_val:
-                max_val = val
-        return max_val  
+# class Solution:
+#     def largest(self, arr):   
+#         max_val = arr[0]
+#         for val in arr:
+#             if val > max_val:
+#                 max_val = val
+#         return max_val  
     
 
-s = Solution()
-print(s.largest([1, 8, 7, 56, 90]))
+# s = Solution()
+# print(s.largest([1, 8, 7, 56, 90]))
 
     
+# l = [1,2,2,4]
+# print(l)
+# l1=[]
+# for i in l:
+#     if i not in l1:
+#         l1.append(i)
+# print(l1)        
