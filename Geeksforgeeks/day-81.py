855a9fb055cafc52aa07640ea977e85c171087f7
kunalaswar <kunalaswar2004@gmail.com>
2026-02-10 21:59:16 +0530

    day-81

--- Geeksforgeeks/day-81.py
+++ Geeksforgeeks/day-81.py
@@ -1,2 +1,20 @@
 
-#
+# Check if divisible by 11
+class Solution:
+    def divisibleBy11(self, S):
+        S = str(S) 
+        odd_sum = 0
+        even_sum = 0
+
+        for i in range(len(S)):
+            if i % 2 == 0:      
+                odd_sum += int(S[i])
+            else:
+                even_sum += int(S[i])
+
+        if abs(odd_sum - even_sum) % 11 == 0:
+            return 1
+        else:
+            return 0
+s = Solution()
+print(s.divisibleBy11(76945))
