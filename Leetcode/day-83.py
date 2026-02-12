661c2f51b3fdb541e2778716b13ed2d558d8ebf9
kunalaswar <kunalaswar2004@gmail.com>
2026-02-12 08:07:12 +0530

    day-83

--- Leetcode/day-83.py
+++ Leetcode/day-83.py
@@ -1,1 +1,26 @@
-#
+# 5. Longest Palindromic Substring
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+    
+        
+        def expand(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+
+        longest = ""
+
+        for i in range(len(s)):
+            # Odd length palindrome
+            p1 = expand(i, i)
+            # Even length palindrome
+            p2 = expand(i, i + 1)
+
+            longest = max(longest, p1, p2, key=len)
+
+        return longest
+
+obj = Solution()
+result = obj.longestPalindrome("babad")
+print(result)
