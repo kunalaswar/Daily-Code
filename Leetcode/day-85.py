f2f9bfef20727b6f6244f0a46fd5b62d483a324e
kunalaswar <kunalaswar2004@gmail.com>
2026-02-16 07:50:24 +0530

    day-85

---
+++ Leetcode/day-85.py
@@ -0,0 +1,60 @@
+# 21. Merge Two Sorted Lists
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode(0)
+        current = dummy
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+
+        current.next = list1 if list1 else list2
+        return dummy.next
+
+
+# Helper function to create a linked list from a Python list
+def create_linked_list(values):
+    dummy = ListNode(0)
+    current = dummy
+    for val in values:
+        current.next = ListNode(val)
+        current = current.next
+    return dummy.next
+
+
+# Helper function to print linked list
+def print_linked_list(head):
+    result = []
+    while head:
+        result.append(head.val)
+        head = head.next
+    print(result)
+
+
+
+
+list1 = create_linked_list([1, 2, 4])
+list2 = create_linked_list([1, 3, 4])
+
+solution = Solution()
+merged_head = solution.mergeTwoLists(list1, list2)
+
+print_linked_list(merged_head)
