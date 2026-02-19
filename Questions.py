4c515690e62f6645743b98583f9c26ce8f8e365e
kunalaswar <kunalaswar2004@gmail.com>
2026-02-19 21:23:13 +0530

    update

--- Questions.py
+++ Questions.py
@@ -6,6 +6,20 @@
 # ["h","e","l","l","o"] → ["o","l","l","e","h"]
 
 s = ["h","e","l","l","o"]
+# print(s[::-1])
+for i  in range(len(s)):
+    print(s[i])
+lst = [val for val in s]    
+print(lst)
+
+
+
+
+
+
+    
+
+    
 
 
 
