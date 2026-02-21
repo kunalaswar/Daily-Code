e2dc66f8506e275f4fcd3148907ecd85281d2cdc
kunalaswar <kunalaswar2004@gmail.com>
2026-02-21 09:37:45 +0530

    Q-2

--- Questions.py
+++ Questions.py
@@ -17,8 +17,35 @@
 # print(s)
 
 # -----------------------------------------------------------
+#! 2. Valid Palindrome
+# Return True if a string is a palindrome,
+# considering only alphanumeric characters and ignoring case.
+# Do not use slicing (s[::-1]).
+# Example:
+# "A man, a plan, a canal: Panama" → True
+
+s = "A man, a plan, a canal: Panama"
+left = 0
+right = len(s)-1
+# print(left , right)
+while(left<right):
+    # "Keep skipping until character becomes valid." that why we use the while 
+    while left <right and not s[left].isalnum(): # amanaplanacanalpanama
+        left += 1
+    while left < right and not s[right].isalnum():
+        right -= 1 
+    if s[left].lower() !=  s[right].lower():
+        print(False)
+        break 
+
+    left += 1 
+    right -= 1 
+else:
+    print(True)    
 
     
+
+
 
 
 
