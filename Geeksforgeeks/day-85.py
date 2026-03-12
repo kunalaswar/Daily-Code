3eca96f1a406762885044bf2597101e4b8e3cc34
Kunal Aswar <kunalaswar2004@gmail.com>
2026-03-12 21:59:01 +0530

    update

--- Geeksforgeeks/day-85.py
+++ Geeksforgeeks/day-85.py
@@ -1,41 +1,41 @@
-# Non Repeating Character
+# # Non Repeating Character
 
-# Given a string s consisting of lowercase English Letters. return the first non-repeating character in s. If there is no non-repeating character, return '$'.
+# # Given a string s consisting of lowercase English Letters. return the first non-repeating character in s. If there is no non-repeating character, return '$'.
 
-# Examples:
+# # Examples:
 
-# Input: s = "geeksforgeeks"
-# Output: 'f'
-# Explanation: In the given string, 'f' is the first character in the string which does not repeat.
-# Input: s = "racecar"
-# Output: 'e'
-# Explanation: In the given string, 'e' is the only character in the string which does not repeat.
-# Input: s = "aabbccc"
-# Output: '$'
-# Explanation: All the characters in the given string are repeating.
+# # Input: s = "geeksforgeeks"
+# # Output: 'f'
+# # Explanation: In the given string, 'f' is the first character in the string which does not repeat.
+# # Input: s = "racecar"
+# # Output: 'e'
+# # Explanation: In the given string, 'e' is the only character in the string which does not repeat.
+# # Input: s = "aabbccc"
+# # Output: '$'
+# # Explanation: All the characters in the given string are repeating.
 
 
-def nonRepeatingChar(s):
+# def nonRepeatingChar(s):
     
-    freq = {}   # dictionary to store character count
+#     freq = {}   # dictionary to store character count
     
-    # Step 1: count frequency
-    for ch in s:
-        if ch in freq:
-            freq[ch] += 1
-        else:
-            freq[ch] = 1
+#     # Step 1: count frequency
+#     for ch in s:
+#         if ch in freq:
+#             freq[ch] += 1
+#         else:
+#             freq[ch] = 1
 
-    # Step 2: find first non-repeating
-    for ch in s:
-        if freq[ch] == 1:
-            return ch
+#     # Step 2: find first non-repeating
+#     for ch in s:
+#         if freq[ch] == 1:
+#             return ch
 
-    return '$'   
+#     return '$'   
 
-print(nonRepeatingChar("racecar"))
-print(nonRepeatingChar( "geeksforgeeks"))
-print(nonRepeatingChar("aabbccc"))
+# print(nonRepeatingChar("racecar"))
+# print(nonRepeatingChar( "geeksforgeeks"))
+# print(nonRepeatingChar("aabbccc"))
 
         
     
