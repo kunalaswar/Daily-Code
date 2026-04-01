a72a00d1e8ff55722d3268fc7f6a015f9869b6b2
Kunal Aswar <kunalaswar2004@gmail.com>
2026-04-01 14:22:49 +0530

    Q-7

--- Questions.py
+++ Questions.py
@@ -270,6 +270,33 @@ nums = [0,1,0,3,12]
 # print("profit : ",max_profit)         
 
 
+#===============================================================
+#! 7. merge sorted array 
+#  Marge two sorted arrays into one sorted array in place 
+
+a = [1,2,3,0,0,0]
+b = [2,5,6]
+
+m = 3
+n = 3
+i = m-1
+j = n-1
+k = m + n - 1
+while(i>=0 and j>=0):
+    if a[i]>b[j]:
+        a[k] = a[i]
+        i = i-1
+    else:
+        a[k]=b[j]
+        j = j-1
+    k = k - 1 
+
+while(j>=0):
+    a[k] = b[j]
+    j = j-1
+    k = k-1
+print(a)   
+ 
 
 
 
