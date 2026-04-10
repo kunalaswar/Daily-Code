d0e3a6190e9029ab93c83aab480563b9844c2df8
Kunal Aswar <kunalaswar2004@gmail.com>
2026-04-10 11:06:55 +0530

    Q-9

--- Questions.py
+++ Questions.py
@@ -316,14 +316,39 @@ nums = [0,1,0,3,12]
 
 
 # boyers more voting 
-lst = [3,2,3]
-candidate = None
-count = 0
-for num in lst:
-    if count ==0:
-        candidate = num 
-    count +=1 if num == candidate  else -1 
-print(candidate)
+# lst = [3,2,3]
+# candidate = None
+# count = 0
+# for num in lst:
+#     if count ==0:
+#         candidate = num 
+#     count +=1 if num == candidate  else -1 
+# print(candidate)
+
+#===============================================================
+#! 9. Intersection of two Arraya
+# Return the intersection of two arrays continue elements 
+# Eaxmples
+
+# lst1 = [1,2,2,1]
+# lst2 = [2,2]
+# common_element = set()
+# for i in lst1:
+#     if i in lst2:
+#         common_element.add(i)
+# print(common_element)   
+
+# 
+lst1 = [1,2,2,1]
+lst2 = [2,2]
+set2 = set(lst2)
+common_element = set()
+for i in lst1:
+    if i in set2:
+        common_element.add(i)
+print(common_element)        
+
+
 
 
 
