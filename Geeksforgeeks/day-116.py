3e107c1a0a47a112d28859ba77ddbd87832d2b74
Kunal Aswar <kunalaswar2004@gmail.com>
2026-05-17 11:08:49 +0530

    Q-116

---
+++ Geeksforgeeks/day-116.py
@@ -0,0 +1,29 @@
+# Product of an Array
+# Given an array, arr[] of positive integers. Your task is to return the product of array elements under the given modulo, mod with the value of 1000000007.
+
+# Note: The modulo operation finds the remainder after the division of one number by another. For example, k(mod(m))=k%m= remainder obtained when k is divided by m
+
+# Examples:
+
+# Input: arr[] = [1, 2, 3, 4]
+# Output: 24
+# Explanation: The product of the elements in the array is 1×2×3×4=24. Since 24 is less than 1000000007, the output is simply 24.
+# Input: arr[] = [100000, 100000, 100000]
+# Output: 993000007
+# Explanation:  The product of the array elements is 100000 × 100000 × 100000 = 1000000000000000. Taking modulo 1000000007, the result is 1000000000000000 % 1000000007 = 993000007
+
+class Solution:
+
+    def product(self, arr):
+
+        mul = 1
+
+        for i in arr:
+
+            mul = mul * i
+
+        return mul % 1000000007  
+
+s = Solution()        
+print(s.product([1, 2, 3, 4]))
+print(s.product([100000, 100000, 100000]))
