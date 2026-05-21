87ade673e2d9afeb1753a09e922a6efd7119353a
Kunal Aswar <kunalaswar2004@gmail.com>
2026-05-21 08:36:02 +0530

    Q-120

--- Geeksforgeeks/day-119.py
+++ Geeksforgeeks/day-119.py
@@ -6,7 +6,15 @@
 #         for i in range(len(arr1)):
 #             for j in range(len(arr2)-1):
 #                 if arr1[i]+arr2[j]==target:
+
                     # return arr1[i] ,arr2[j]
+# s = Solution()
+# print(s.allPairs(9 ,[1, 2, 4, 5, 7],[5, 6, 3, 4, 8]))
+# print(s.allPairs( 8, [-1, -2, 4, -6, 5, 7], [6, 3, 4, 0]))
+# print(s.allPairs( 9, [1, 2, 4, 5, 7, 4],[5, 6, 3, 4, 8, 4]))
+
+
+
 class Solution:
     def allPairs(self, target, arr1, arr2):
         lst = []
@@ -19,6 +27,9 @@ class Solution:
 
 s = Solution()
 print(s.allPairs(9 ,[1, 2, 4, 5, 7],[5, 6, 3, 4, 8]))
+print(s.allPairs( 8, [-1, -2, 4, -6, 5, 7], [6, 3, 4, 0]))
+print(s.allPairs( 9, [1, 2, 4, 5, 7, 4],[5, 6, 3, 4, 8, 4]))
+#
 
 class Solution:
     def allPairs(self, target, arr1, arr2):
---
+++ Geeksforgeeks/day-120.py
@@ -0,0 +1,33 @@
+# Buildings with Sunlight
+# Given the array arr[] of heights of certain buildings that lie adjacent to each other, Sunlight starts falling from the left side of the buildings. If there is a building of a certain height, all the buildings to the right side of it having lesser heights cannot see the sun. Find the total number of buildings that receive sunlight.
+
+# Note: Buildings with equal height do not block each other and can also receive sunlight.
+
+# Examples:
+# Input: arr[] = [6, 2, 8, 4, 11, 13]
+# Output: 4
+# Explanation: Only the buildings with heights 6, 8, 11, and 13 receive sunlight; therefore, the output is 4.
+  
+# Input: arr[] = [2, 5, 1, 8, 3]
+# Output: 3
+# Explanation: Only buildings of height 2, 5 and 8 can see the sun, hence output is 3.
+  
+# Input: arr[] = [3, 3, 3, 1]
+# Output: 3
+# Explanation: The first three buildings (height 3) receive sunlight, while the last building (1) is blocked. Hence, the output is 3.
+
+class Solution:
+    def visibleBuildings(self, arr):
+        count  = 0 
+        max_height = 0
+        for build in arr:
+            if build >= max_height:
+                count = count + 1 
+                max_height = build
+        return count
+
+s= Solution()           
+print(s.visibleBuildings([6, 2, 8, 4, 11, 13]))
+print(s.visibleBuildings([2, 5, 1, 8, 3]))
+print(s.visibleBuildings([3, 3, 3, 1]))
+
