7747977017a47a04171c048ba5b1fa912173966e
Kunal Aswar <kunalaswar2004@gmail.com>
2026-06-05 07:30:49 +0530

    Update

--- Geeksforgeeks/day-131.py
+++ Geeksforgeeks/day-131.py
@@ -19,14 +19,14 @@
 
 
 
-# def makeBeautiful(arr):
-#     stack = []
-#     for num in arr:
-#         if stack and ((stack[-1] >= 0 and num < 0) or
-#                           (stack[-1] < 0 and num >= 0)):
-#             stack.pop()
-#         else:
-#             stack.append(num)
-#     return stack
-# print(makeBeautiful([4, 2,-2, 1]))
-# print(makeBeautiful([2,-2, -1, 1]))
+def makeBeautiful(arr):
+    stack = []
+    for num in arr:
+        if stack and ((stack[-1] >= 0 and num < 0) or
+                          (stack[-1] < 0 and num >= 0)):
+            stack.pop()
+        else:
+            stack.append(num)
+    return stack
+print(makeBeautiful([4, 2,-2, 1]))
+print(makeBeautiful([2,-2, -1, 1]))
