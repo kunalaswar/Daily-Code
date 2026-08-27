8972c703695b588ab1f620e65636ef6595543f65
Kunal Aswar <kunalaswar2004@gmail.com>
2026-08-27 21:40:29 +0530

    Q-146

---
+++ Geeksforgeeks/day-146.py
@@ -0,0 +1,26 @@
+#Regex - Python
+
+
+# Regular expressions are extremely helpful in extracting useful information from loads of text. Regular expressions work on pattern-matching techniques. Extracting phone-number, validating passwords, and extracting images from web-pages are but a few examples of regex usage.
+
+# In this question, we'll learn the use of Regex in Python. You will be provided a string str in which you have to find all the numbers and print them.
+
+# Note: In Python, you need to import re module to use regex
+# Example:
+# Input: 
+# str = asdasd123asmdasdk34234kfdsd34sdfk5
+# Output: 
+# 123 34234 34 5
+
+import re
+def numberMatcher(str):
+    pat = r'\d+'
+    match = re.findall(pat, str)
+
+    if match:
+        for i in match:
+            print(i, end=" ")
+    else:
+        print(-1, end="")
+
+numberMatcher("asdasd123asmdasdk34234kfdsd34sdfk5")     
