085aae21c3dd4e558caaf2a644ebfda1ecaded4a
Kunal Aswar <kunalaswar2004@gmail.com>
2026-09-09 20:31:52 +0530

    Q-148

---
+++ Geeksforgeeks/day-148.py
@@ -0,0 +1,16 @@
+# While Loop
+# Given a number x, print the numbers from x to 0 in decreasing order in a single line.
+
+# Examples:
+# Input: x = 3
+# Output: 3 2 1 0
+# Explanation: Numbers in decreasing order from 3 are 3 2 1 0.
+# Input: x = 5
+# Output: 5 4 3 2 1 0
+# Explanation: Numbers in decreasing order from 5 are 5 4 3 2 1 0.
+
+x = int(input("Enter number here : "))
+while x >= 0:
+    print(x, end=" ")
+    x -= 1
+
